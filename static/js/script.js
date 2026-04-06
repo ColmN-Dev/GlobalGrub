838958cedbc97c66f0bc25dbab1648619ae8fa82
Colm Nolan <182516888+ColmN-Dev@users.noreply.github.com>
2026-04-06 19:42:23 +0100

    Implement toggleMenu function for mobile menu
    Added toggleMenu function to manage mobile menu state.

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,7 @@ const mobileMenu = document.getElementById('mobileMenu');
 hamburger.addEventListener('click', () => {
     mobileMenu.classList.toggle('open');
 });
+
+function toggleMenu() {
+    document.querySelector('.mobile-menu').classList.toggle('open');
+}
